9ba2df64d31386c765f4996b5a4a481c534bbf3d
Jamie Doyle <thedoyles922@users.noreply.github.com>
2017-06-16 15:59:30 -0400

    added styling

--- js/app.js
+++ js/app.js
@@ -5,26 +5,43 @@
   var myHeaders = {};
   var userUrl = 'https://api.github.com/users/TheDoyles922';
   var reposUrl = 'https://api.github.com/users/TheDoyles922/repos?sort=updated';
+  var orgUrl = 'https://api.github.com/user/orgs';
   var profileNode = document.getElementById('profile');
   var repoNode = document.getElementById('repos');
   var repoTotal = document.getElementById('repo-total');
-  if (TOKEN) {
-      // set the AJAX header to send the token
-      myHeaders.Authorization = 'token ' + TOKEN;
+  var orgSection = document.getElementById('orgs');
+
+  try {
+    myHeaders['Authorization'] = 'token ' + TOKEN;
+  } catch (e) {
+    //ignore error
   }
+
+  // if (TOKEN) {
+  //     // set the AJAX header to send the token
+  //     myHeaders.Authorization = 'token ' + TOKEN;
+  // }
 
   fetch(userUrl, {headers: myHeaders}).then(function(res){
     res.json().then(function (data) {
       console.log(data);
+      var iconSpan = document.createElement('span');
       var avatar = document.createElement('div');
+      var emailDiv = document.createElement('div');
       var location = document.createElement('h5');
-      var login = document.createElement('h5');
+      var login = document.createElement('h3');
       var email = document.createElement('a');
       var name = document.createElement('h2');
       var bio = document.createElement('p');
+      var locationIconContainer = document.createElement('div');
+      var locationImage = document.createElement('img');
+      locationImage.src = 'images/location-image.png';
+      locationImage.classList.add('location-image');
       avatar.innerHTML = '<img src="' + data.avatar_url + '">';
       avatar.classList.add('image');
-      location.textContent = data.location;
+      iconSpan.setAttribute('class', 'fa fa-envelope-o');
+      location.textContent = ' ' + data.location;
+      locationIconContainer.classList.add('location-container');
       login.textContent = data.login;
       email.textContent = data.email;
       email.href = data.email;
@@ -34,8 +51,12 @@
       profileNode.appendChild(name);
       profileNode.appendChild(login);
       profileNode.appendChild(bio);
-      profileNode.appendChild(location);
-      profileNode.appendChild(email);
+      profileNode.appendChild(locationIconContainer);
+      locationIconContainer.appendChild(locationImage);
+      locationIconContainer.appendChild(location);
+      profileNode.appendChild(emailDiv);
+      emailDiv.appendChild(iconSpan);
+      emailDiv.appendChild(email);
     });
   });
 
@@ -49,6 +70,29 @@
       }
     });
   });
+
+  fetch(orgUrl, {headers: myHeaders}).then(function(resp){
+    resp.json().then(function (data3) {
+      // if no orgazizations, do not show org section
+      if (data3.length === 0 ) {
+        return;
+      }
+      // create org title
+      var orgTitle = document.createElement('h4');
+      orgTitle.textContent = 'Organizations';
+      orgSection.appendChild(orgTitle);
+      data3.forEach(function(){
+        var orgImgNode = document.createElement('span');
+        orgImgNode.setAttribute('class', 'org-image');
+        var orgImg = 'img src="' + data3[0].avatar_url + '">';
+        orgImgNode.innerHTML = orgImg;
+        console.log(orgImg);
+        orgSection.appendChild(orgImgNode);
+
+      });
+    });
+  });
+
   function displayRepo(repo) {
     var repoDiv = document.createElement('div');
     var repoCont = document.createElement('div');
